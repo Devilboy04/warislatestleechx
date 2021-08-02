a293e834a327b31e7de223476eb9d9f4260b547d
indrabushan <72606082+indrabushan@users.noreply.github.com>
2021-08-02 13:52:27 +0530

    Start disabled

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -193,22 +193,22 @@ if __name__ == "__main__":
     )
     app.add_handler(upload_log_handler)
     #
-    help_text_handler = MessageHandler(
-        help_message_f,
-        filters=filters.command([f"{HELP_COMMAND}"]) & filters.chat(chats=AUTH_CHANNEL),
-    )
-    app.add_handler(help_text_handler)
+    #help_text_handler = MessageHandler(
+    #    help_message_f,
+    #    filters=filters.command([f"{HELP_COMMAND}"]) & filters.chat(chats=AUTH_CHANNEL),
+    #)
+    #app.add_handler(help_text_handler)
     #
     new_join_handler = MessageHandler(
         new_join_f, filters=~filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(new_join_handler)
     #
-    group_new_join_handler = MessageHandler(
-        help_message_f,
-        filters=filters.chat(chats=AUTH_CHANNEL) & filters.new_chat_members,
-    )
-    app.add_handler(group_new_join_handler)
+    #group_new_join_handler = MessageHandler(
+    #    help_message_f,
+    #    filters=filters.chat(chats=AUTH_CHANNEL) & filters.new_chat_members,
+    #)
+    #app.add_handler(group_new_join_handler)
     #
     call_back_button_handler = CallbackQueryHandler(button)
     app.add_handler(call_back_button_handler)
